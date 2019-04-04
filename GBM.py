9b1e1e6ae39af793dc5940192df8abbe97e4f025
Max <a@a.com>
2019-04-04 23:11:22 +0100

    Functionised, added multiple path functionality, also option to plot and/or save to csv

--- GBM.py
+++ GBM.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def Brownian(seed, N):
     np.random.seed(seed)
     dt = 1. / N  # time step
-    b = np.random.normal(0., 1., int(N)) * np.sqrt(dt)  # brownian increments
-    W = np.cumsum(b)  # brownian path
+    b = np.random.normal(0, 1, int(N)) * np.sqrt(dt)  # Brownian increments
+    W = np.cumsum(b)  # Brownian path
     return W, b
-
 
 # GBM Exact Solution
 
@@ -21,30 +22,49 @@ def Brownian(seed, N):
 # N:      number of increments
 
 
-def GBM(So, mu, sigma, W, N):
+def GBM(s0, mu, sigma, W, N, t):
     S = []
-    S.append(So)
+    S.append(s0)
     for i in range(1, int(N+1)):
         drift = (mu - 0.5 * sigma**2) * t[i]
         diffusion = sigma * W[i-1]
-        S_temp = So*np.exp(drift + diffusion)
+        S_temp = s0*np.exp(drift + diffusion)
         S.append(S_temp)
-    return S, t
+    return S
 
 
-seed = np.random.randint(10000)
-N = 100000
-So = 1
-mu = 0
-sigma = 0.4
+def make_GBM_paths(paths=10, N=500, mu_range=(-0.5, 0.5), sigma_range=(0.1, 0.5), plot=False, save=True):
+    mu = np.random.uniform(mu_range[0], mu_range[1], paths)
+    sigma = np.random.uniform(sigma_range[0], sigma_range[1], paths)
+
+    s0 = 1
+    params = pd.DataFrame(columns=['mu', 'sigma', 'seed'])
+    GBM_path = pd.DataFrame()
+    for mu, sigma in zip(mu, sigma):
+        seed = np.random.randint(10000)
+
+        mu = mu
+        sigma = sigma
+        param_dict_add = {'mu': mu, 'sigma': sigma, 'seed': int(seed)}
+        params = params.append(param_dict_add, ignore_index=True)
+
+        w = Brownian(seed, N)[0]
+        t = np.linspace(0, 1, N + 1)
+        soln = GBM(s0, mu, sigma, w, N, t)   # Exact solution
+        GBM_path = GBM_path.append(pd.DataFrame([soln]), ignore_index=True)
+
+        if plot:
+            plt.plot(t, soln, linewidth=0.5, label=str(np.round(mu, 2)) + ":" + str(np.round(sigma, 2)))
+            plt.ylabel('Stock Price, $')
+            plt.title('Geometric Brownian Motion')
+
+    if save:
+        params.to_csv('params.csv', sep='\t', index=False)
+        GBM_path.to_csv('GBM_paths.csv', sep='\t', index=False, header=False)
+
+    if plot:
+        plt.legend(fontsize='small')
+        plt.show()
 
 
-W = Brownian(seed, N)[0]
-t = np.linspace(0, 1, N + 1)
-soln = GBM(So, mu, sigma, W, N)[0]    # Exact solution
-
-
-plt.plot(t, soln, linewidth=0.75)
-plt.ylabel('Stock Price, $')
-plt.title('Geometric Brownian Motion')
-plt.show()
+make_GBM_paths(plot=True)
